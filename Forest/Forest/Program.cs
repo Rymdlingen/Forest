88088cf9ca64a1210b72d51688538cb3eaaa3a2f
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2020-11-03 17:59:17 +0100

    Title screen/art and introduction
    The files are added and read into the project but not with the right texts. The program displays the title screen/art and the first part of the story. Added method for printing out the text.

--- Forest/Forest/Program.cs
+++ Forest/Forest/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace Forest
 {
     class Program
     {
+        const ConsoleColor NarrativeColor = ConsoleColor.Gray;
+        const ConsoleColor PromptColor = ConsoleColor.White;
+        const int PrintPauseMilliseconds = 150;
+
+        static void Print(string text)
+        {
+            // Split text into lines that don't exceed the window width.
+            int maximumLineLength = Console.WindowWidth - 1;
+            MatchCollection lineMatches = Regex.Matches(text, @"(.{1," + maximumLineLength + @"})(?:\s|$)");
+
+            // Output each line with a small delay.
+            foreach (Match line in lineMatches)
+            {
+                Console.WriteLine(line.Groups[0].Value);
+                Thread.Sleep(PrintPauseMilliseconds);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Displaying title art
+            string titleArtPath = "ForestTitleArt.txt";
+            string[] title = File.ReadAllLines(titleArtPath);
+            foreach (string line in title)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            // Displaying the introduction/first part of the games story
+            string storyPath = "ForestGameStory.txt";
+            string[] gameStory = File.ReadAllLines(storyPath);
+            Print(gameStory[0]);
         }
     }
 }
