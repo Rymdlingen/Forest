468e30d13017550b3968cc4bc734279188b49fb9
Rymdlingen <71494058+Rymdlingen@users.noreply.github.com>
2020-11-04 11:59:04 +0100

    Asking for command and evaluating
    The game asks for a command and the player can write in the console. Added a switch that will recognise specified verbs, directions and quit. Will need to add more verbs later and add what happens if one of the of the recognisable words are entered.

--- Forest/Forest/Program.cs
+++ Forest/Forest/Program.cs
@@ -11,6 +11,8 @@ namespace Forest
         const ConsoleColor PromptColor = ConsoleColor.White;
         const int PrintPauseMilliseconds = 150;
 
+        static bool quitGame = false;
+
         static void Print(string text)
         {
             // Split text into lines that don't exceed the window width.
@@ -22,6 +24,65 @@ namespace Forest
             {
                 Console.WriteLine(line.Groups[0].Value);
                 Thread.Sleep(PrintPauseMilliseconds);
+            }
+        }
+
+        static void Reply(string text)
+        {
+            Print(text);
+            Console.WriteLine();
+        }
+
+        static void HandlePlayerAction()
+        {
+            // Ask player what they want to do.
+            Console.ForegroundColor = NarrativeColor;
+            Print("Whats next?");
+
+            Console.ForegroundColor = PromptColor;
+            Console.Write("> ");
+
+            string command = Console.ReadLine().ToLowerInvariant();
+
+            // Split the command into words.
+            string[] words = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            // Assuming the first word in the command is a verb.
+            string verb = words[0];
+
+            // Call the right handler for the given verb.
+            switch (verb)
+            {
+                case "north":
+                case "n":
+                    // TODO
+                    break;
+                case "south":
+                case "s":
+                    // TODO
+                    break;
+                case "west":
+                case "w":
+                    // TODO
+                    break;
+                case "east":
+                case "e":
+                    // TODO
+                    break;
+
+                case "quit":
+                case "q":
+                case "end":
+                case "exit":
+                    // TODO
+                    {
+                        quitGame = true;
+                    }
+                    break;
+                default:
+                    // TODO
+                    Reply("I don't understand");
+                    break;
             }
         }
 
@@ -38,9 +99,20 @@ namespace Forest
             Console.Clear();
 
             // Displaying the introduction/first part of the games story
+            Console.ForegroundColor = NarrativeColor;
             string storyPath = "ForestGameStory.txt";
             string[] gameStory = File.ReadAllLines(storyPath);
             Print(gameStory[0]);
+            Console.WriteLine();
+
+            // Display short instructions about how to play??
+
+            // Game loop
+            while (!quitGame)
+            {
+                // Ask player what they want to do.
+                HandlePlayerAction();
+            }
         }
     }
 }
